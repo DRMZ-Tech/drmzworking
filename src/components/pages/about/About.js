108630f49764b8d9958ca721c09dc0249db0543c
Lalit Thakre <lalit03thakre@gmail.com>
2023-06-05 17:00:48 +0530

    commit

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -1,8 +1,17 @@
 import React from "react";
-import "../../App.css";
+import "../../../App.css";
 
 function About() {
-  return <h1 className="about">About</h1>;
+  return (
+    <h1 className="about">
+      CONTACT US Drmz System Innovations Drmz System Innovations is a registered
+      Pvt. Ltd. Company. We at DRMZ, are a team of creative, enthusiastic &
+      focused people. We are committed to enhance research work across India.
+      Our effort will be to provide young researchers a suitable environment &
+      technical facilities to nurture their creative & research oriented
+      mindset..
+    </h1>
+  );
 }
 
 export default About;
