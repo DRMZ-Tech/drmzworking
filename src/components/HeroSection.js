e8e6ab9d25f8364d85bbaa714e93c26d36890fa8
Lalit Thakre <lalit03thakre@gmail.com>
2023-06-06 10:31:50 +0530

    commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,21 +5,26 @@ import "../App.css";
 
 export default function HeroSection() {
   return (
-    <div className="hero-container">
-      <h1>Welcome to the DRMZ Systems</h1>
-      <p>
-        Industrial Automation Simplified We manufacture smart products to help
-        industries transition into Industry 4.0
-      </p>
-      <div className="hero-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-          link="/"
-        >
-          Get Started
-        </Button>
+    <div className="main-container">
+      <div className="hero-container">
+        <h1>Industrial Automation Simplified</h1>
+        <p>
+          We manufacture smart products to help industries transition into
+          Industry 4.0
+        </p>
+        <div className="hero-btns">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+            link="/"
+          >
+            Welcome
+          </Button>
+        </div>
+      </div>
+      <div className="hero-container1">
+        <img className="photo" src="homePageImage.png" alt="logo" />
       </div>
     </div>
   );
