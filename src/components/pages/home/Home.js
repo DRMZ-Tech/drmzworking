3c84c1181917052fcfa3fb23d5c66d8b6fe5adf6
Lalit Thakre <lalit03thakre@gmail.com>
2023-06-06 16:58:31 +0530

    new images added in home page

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -5,26 +5,66 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="main-container">
-      <div className="hero-container">
-        <h1>Industrial Automation Simplified</h1>
-        <p>
-          We manufacture smart products to help industries transition into
-          Industry 4.0
-        </p>
-        <div className="hero-btns">
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-            link="/"
-          >
-            Welcome
-          </Button>
+    <div>
+      <div className="main-container">
+        <div className="hero-container">
+          <h1>Industrial Automation Simplified</h1>
+          <p>
+            We manufacture smart products to help industries transition into
+            Industry 4.0
+          </p>
+          <div className="hero-btns">
+            <Button
+              className="btns"
+              buttonStyle="btn--outline"
+              buttonSize="btn--large"
+              link="/"
+            >
+              Welcome
+            </Button>
+          </div>
+        </div>
+        <div className="hero-container1">
+          <img className="photo" src="homePageImage.png" alt="logo" />
         </div>
       </div>
-      <div className="hero-container1">
-        <img className="photo" src="homePageImage.png" alt="logo" />
+      <div className="image-grid">
+        <div className="grid-item">
+          <img
+            src="https://drmz.in/assets/img/clients/client-1.png"
+            alt="Image 1"
+          />
+        </div>
+        <div className="grid-item">
+          <img
+            src="https://drmz.in/assets/img/clients/client-2.png"
+            alt="Image 2"
+          />
+        </div>
+        <div className="grid-item">
+          <img
+            src="https://drmz.in/assets/img/clients/client-3.png"
+            alt="Image 3"
+          />
+        </div>
+        <div className="grid-item">
+          <img
+            src="https://drmz.in/assets/img/clients/client-4.png"
+            alt="Image 4"
+          />
+        </div>
+        <div className="grid-item">
+          <img
+            src="https://drmz.in/assets/img/clients/client-5.png"
+            alt="Image 5"
+          />
+        </div>
+        <div className="grid-item">
+          <img
+            src="https://drmz.in/assets/img/clients/client-6.png"
+            alt="Image 6"
+          />
+        </div>
       </div>
     </div>
   );
