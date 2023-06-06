6b1af1d05f166442872e9d8c1207509a58d79dde
Lalit Thakre <lalit03thakre@gmail.com>
2023-06-06 18:03:58 +0530

    commit

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,7 +1,30 @@
 import React from "react";
 import "../../../App.css";
+import "./Contact.css";
 
 function Contact() {
-  return <h1 className="contact">Contact</h1>;
+  return (
+    <div className="contact-info">
+      <div className="div1">
+        <h2>Drmz System Innovations</h2>
+        <p>
+          Drmz System Innovations is a registered Pvt. Ltd. Company. We at DRMZ,
+          are a team of creative, enthusiastic & focused people. We are
+          committed to enhance research work across India. Our effort will be to
+          provide young researchers a suitable environment & technical
+          facilities to nurture their creative & research oriented mindset.
+        </p>
+      </div>
+      <div className="div2">
+        <p>
+          First Floor, Plot 69B, Kasturba Nagar
+          <br />
+          Bhopal, MP 462023
+        </p>
+        <p>Email: contact@drmztech.com</p>
+        <p>Phone: +91-7581806218</p>
+      </div>
+    </div>
+  );
 }
 export default Contact;
