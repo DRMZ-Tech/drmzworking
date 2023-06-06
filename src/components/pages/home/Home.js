c9aba576b2fc2ec061506edb2486a7298cf0f884
Lalit Thakre <lalit03thakre@gmail.com>
2023-06-06 16:30:38 +0530

    commit

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,11 +1,32 @@
 import "../../../App.css";
-import HeroSection from "../../HeroSection";
+import React from "react";
+import { Button } from "../../Button";
+import "./Home.css";
 
 function Home() {
   return (
-    <>
-      <HeroSection />
-    </>
+    <div className="main-container">
+      <div className="hero-container">
+        <h1>Industrial Automation Simplified</h1>
+        <p>
+          We manufacture smart products to help industries transition into
+          Industry 4.0
+        </p>
+        <div className="hero-btns">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+            link="/"
+          >
+            Welcome
+          </Button>
+        </div>
+      </div>
+      <div className="hero-container1">
+        <img className="photo" src="homePageImage.png" alt="logo" />
+      </div>
+    </div>
   );
 }
 
