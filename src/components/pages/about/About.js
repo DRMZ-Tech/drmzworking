f4d219709b7584c72f0c37c26fc0ebee5d64d0e6
luckydarwai <luckydarvai545@gmail.com>
2023-06-07 11:06:55 +0530

    about,contectform done

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -1,18 +1,49 @@
 import React from "react";
 import "../../../App.css";
 import "./About.css";
-
-function About() {
+const About = () => {
   return (
-    <h1 className="about">
-      CONTACT US Drmz System Innovations Drmz System Innovations is a registered
-      Pvt. Ltd. Company. We at DRMZ, are a team of creative, enthusiastic &
-      focused people. We are committed to enhance research work across India.
-      Our effort will be to provide young researchers a suitable environment &
-      technical facilities to nurture their creative & research oriented
-      mindset..
-    </h1>
+    <div className="about">
+        <h1 style={{textAlign:'center'}}>____About US____</h1>
+      <div className="about-content  top-margin">
+        <div>
+          Drmz System Innovations is a registered Pvt. Ltd. Company which provides embedded system and IoT solutions. We aim to provide unique, customized and user friendly solutions and products in the embedded systems space. We at Drmz, are a team of creative, enthusiastic & focused people.
+        </div>
+        <div>
+          The Company is working on many cutting edge technologies and related products like the Internet of things (IoT), Industrial Robotics, Industrial Automation, Home and Office Automation, Demand side energy management system, Smart wireless modules for IoT, Smart attendance system, Smart Solar power generation management system, etc.
+        </div>
+      </div>
+      {/* <img src="champ.jpg" alt="logo" className="champ" /> */}
+      <div className="container">
+        
+        <p>
+          We are a leading organization with several years of experience in our field. Our team has
+          successfully completed numerous projects and has been recognized with prestigious awards.
+          {/* Add more details about your organization */}
+        </p>
+
+        <h3>Awards</h3>
+        <ul>
+          <li>First Place Award - Category X (Year)</li>
+          <li>Best Project Award - Category Y (Year)</li>
+          {/* Add more awards */}
+        </ul>
+
+        <h3>Projects</h3>
+        <ul>
+          <li>Project A - Description of Project A</li>
+          <li>Project B - Description of Project B</li>
+          {/* Add more projects */}
+        </ul>
+
+        <h3>Years of Experience</h3>
+        <p>
+          With over 10 years of experience in the industry, we have successfully delivered high-quality
+          solutions to our clients, gaining their trust and satisfaction.
+        </p>
+      </div>
+    </div>
   );
-}
+};
 
 export default About;
--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,7 +1,70 @@
-import React from "react";
-import "../../../App.css";
 
-function Contact() {
-  return <h1 className="contact">Contact</h1>;
-}
-export default Contact;
+// import "../../../App.css";
+
+import React, { useState } from 'react';
+import "./Contact.css";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission logic or API request with the form data
+    console.log(formData);
+    // Reset the form after submission
+    setFormData({ name: '', email: '', message: '' });
+  };
+
+  return (
+    <section id="contact-form">
+      <div className="container">
+        <h2>Contact Us</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;
